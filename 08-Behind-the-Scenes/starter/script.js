b3d5053868a3edc8f15e0b203af19abd49a32e91
Mary C <mcn4c@virginia.edu>
2021-05-19 13:36:37 -0400

    cloning object

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -29,20 +29,60 @@
 // const year = 2021;
 // const birthYear = 1982;
 
-const jonas = {
-	year: 1991,
-	name: 'Jonas',
-	calcAge: function() {
-		return 2021 - this.year;
-	}
-};
+// const jonas = {
+// 	year: 1991,
+// 	name: 'Jonas',
+// 	calcAge: function() {
+// 		return 2021 - this.year;
+// 	}
+// };
 
-const mary = {
-	year: 1982,
-	name: 'Mary'
-};
+// const mary = {
+// 	year: 1982,
+// 	name: 'Mary'
+// };
 
-//can add method to mary object
-mary.calcAge = jonas.calcAge;
-console.log(jonas.calcAge());
-console.log(mary.calcAge());
+// //can add method to mary object
+// mary.calcAge = jonas.calcAge;
+// console.log(jonas.calcAge());
+// console.log(mary.calcAge());
+
+//******************************************** */
+
+//primitive types
+let lastName = 'Williams';
+let oldLastName = lastName;
+lastName = 'Davis';
+console.log(lastName);
+console.log(oldLastName);
+
+//reference types
+const jessica = {
+	firstName: 'Jessica',
+	lastName: 'Williams',
+	age: 27
+};
+//below code only adds another variable name to same object in the heap
+// const marriedJessica = jessica;
+// marriedJessica.lastName = 'Davis';
+// console.log('Before marriage', jessica);
+// console.log('After marriage', marriedJessica);
+
+//Copying objects
+const jessica2 = {
+	firstName: 'Jessica',
+	lastName: 'Williams',
+	age: 27,
+	family: [ 'Alice', 'Bob' ]
+};
+//Object.assign method creates new object that is an actual copy rather than giving two names to same object
+//only a shallow copy, only first level
+const jessicaCopy = Object.assign({}, jessica2);
+
+//does not change original jessica
+jessicaCopy.lastName = 'Davis';
+//will change the original jessica object because we have not done a deep clone
+jessicaCopy.family.push = 'Mary';
+jessicaCopy.family.push = 'John';
+
+console.log(jessica2, jessicaCopy);
