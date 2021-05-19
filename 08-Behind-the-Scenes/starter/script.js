a23c638f38ac52b1482eada53b23f8bfa3f1bcc9
Mary C <mcn4c@virginia.edu>
2021-05-19 12:05:31 -0400

    created calcAge function to practice scope

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+function calcAge(birthYear) {
+	const age = 2037 - birthYear;
+
+	function printAge() {
+		const output = `You are ${age}, born in ${birthYear}`;
+		console.log(output);
+
+		if (birthYear >= 1981 && birthYear <= 1996) {
+			let string = `You are a millenial, ${firstName}`;
+			console.log(string);
+		} else {
+			let string = `You are not a millenial, ${firstName}`;
+			console.log(string);
+		}
+	}
+	printAge();
+	return age;
+}
+
+const firstName = 'Mary';
+calcAge(1982);
+calcAge(1997);
