902f507f5d284858476204e5d8daa98ee62e173f
Mary C <mcn4c@virginia.edu>
2021-05-19 13:21:41 -0400

    created object with method.  Practice with this

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,24 +1,48 @@
 'use strict';
 
-function calcAge(birthYear) {
-	const age = 2037 - birthYear;
+// function calcAge(birthYear) {
+// 	const age = 2037 - birthYear;
 
-	function printAge() {
-		const output = `You are ${age}, born in ${birthYear}`;
-		console.log(output);
+// 	function printAge() {
+// 		const output = `You are ${age}, born in ${birthYear}`;
+// 		console.log(output);
 
-		if (birthYear >= 1981 && birthYear <= 1996) {
-			let string = `You are a millenial, ${firstName}`;
-			console.log(string);
-		} else {
-			let string = `You are not a millenial, ${firstName}`;
-			console.log(string);
-		}
+// 		if (birthYear >= 1981 && birthYear <= 1996) {
+// 			let string = `You are a millenial, ${firstName}`;
+// 			console.log(string);
+// 		} else {
+// 			let string = `You are not a millenial, ${firstName}`;
+// 			console.log(string);
+// 		}
+// 	}
+// 	printAge();
+// 	return age;
+// }
+
+// const firstName = 'Mary';
+// calcAge(1982);
+// calcAge(1997);
+//********************************************** */
+
+// var me = 'Mary';
+// let job = 'teacher';
+// const year = 2021;
+// const birthYear = 1982;
+
+const jonas = {
+	year: 1991,
+	name: 'Jonas',
+	calcAge: function() {
+		return 2021 - this.year;
 	}
-	printAge();
-	return age;
-}
+};
 
-const firstName = 'Mary';
-calcAge(1982);
-calcAge(1997);
+const mary = {
+	year: 1982,
+	name: 'Mary'
+};
+
+//can add method to mary object
+mary.calcAge = jonas.calcAge;
+console.log(jonas.calcAge());
+console.log(mary.calcAge());
